3e3afaf4c389d533a31406ff11a3a59a20644f57
Jakub Kostial <jakubkostial@eduroam-int-dhcp-97-17-31.ucl.ac.uk>
2023-12-11 15:53:34 +0000

    added formatting loop

--- reshape_matrices_in_folder.py
+++ reshape_matrices_in_folder.py
@@ -14,19 +14,19 @@ folder_path = '/Users/jakubkostial/Documents/phd/code/loop_matmul/repo/matrices/
 
 # Iterate over all files in the directory
 for filename in os.listdir(folder_path):
-    file_path = os.path.join(folder_path, filename)
+    #file_path = os.path.join(folder_path, filename)
     # Check if it's a file, not a directory
-    if os.path.isfile(file_path):
-        # Open and read the file
-        matrix = np.load(filename)
-        # Rescale matrix between 0 and 128
-        rescaled_matrix = rescale_matrix(matrix, 0, 256)
-        # Create a 1920x1080 matrix initialized to 0
-        large_matrix = np.zeros((1080, 1920))
-        # Calculate center coordinates
-        center_x = (large_matrix.shape[1] - rescaled_matrix.shape[1]) // 2
-        center_y = (large_matrix.shape[0] - rescaled_matrix.shape[0]) // 2
-        # Place the rescaled matrix in the center of the larger matrix
-        large_matrix[center_y:center_y + rescaled_matrix.shape[0], center_x:center_x + rescaled_matrix.shape[1]] = rescaled_matrix
-        # Save the array to a CSV file
-        np.savetxt(f'/Users/jakubkostial/Documents/phd/code/loop_matmul/repo/formatted_matrices/slm_ready_{filename}', large_matrix, delimiter=',')
+    # Open and read the file
+    filepath = '/Users/jakubkostial/Documents/phd/code/loop_matmul/repo/matrices/'
+    matrix = np.load(filepath+filename)
+    # Rescale matrix between 0 and 128
+    rescaled_matrix = rescale_matrix(matrix, 0, 256)
+    # Create a 1920x1080 matrix initialized to 0
+    large_matrix = np.zeros((1080, 1920))
+    # Calculate center coordinates
+    center_x = (large_matrix.shape[1] - rescaled_matrix.shape[1]) // 2
+    center_y = (large_matrix.shape[0] - rescaled_matrix.shape[0]) // 2
+    # Place the rescaled matrix in the center of the larger matrix
+    large_matrix[center_y:center_y + rescaled_matrix.shape[0], center_x:center_x + rescaled_matrix.shape[1]] = rescaled_matrix
+    # Save the array to a CSV file
+    np.savetxt(f'/Users/jakubkostial/Documents/phd/code/loop_matmul/repo/formatted_matrices/slm_ready_{filename[:-4]}.csv', large_matrix, delimiter=',')
